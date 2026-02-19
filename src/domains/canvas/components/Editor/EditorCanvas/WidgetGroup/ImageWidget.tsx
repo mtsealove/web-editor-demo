d28840e1d2754f6d8dde5dd660962df33b184ff8
Sanhae Lee <mtsealove0927@gmail.com>
2026-02-19 10:49:03 +0900

    feat: 이미지 cover 형태로 리사이즈 추가

--- src/domains/canvas/components/Editor/EditorCanvas/WidgetGroup/ImageWidget.tsx
+++ src/domains/canvas/components/Editor/EditorCanvas/WidgetGroup/ImageWidget.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Rect, Text, Image as KonvaImage } from 'react-konva';
 import useImage from 'use-image';
 
@@ -9,12 +10,18 @@ interface Props {
 
 function ImageWidget({ w, h, url }: Props) {
     const [img] = useImage(url || '');
+    const crop = useMemo(() => {
+        if (img) {
+            return getCrop(img, w, h);
+        }
+        return {};
+    }, [img, w, h]);
 
     if (img) {
         return <KonvaImage image={img}
             width={w}
             height={h}
-        />;
+            {...crop} />;
     }
     return (
         <>
@@ -22,6 +29,30 @@ function ImageWidget({ w, h, url }: Props) {
             <Text text="🖼" width={w} height={h} fontSize={24} align="center" verticalAlign="middle" />
         </>
     );
+}
+
+function getCrop(image: HTMLImageElement, width: number, height: number) {
+    const aspectRatio = width / height;
+    let newWidth;
+    let newHeight;
+    const imageRatio = image.width / image.height;
+    if (aspectRatio >= imageRatio) {
+        newWidth = image.width;
+        newHeight = image.width / aspectRatio;
+    } else {
+        newWidth = image.height * aspectRatio;
+        newHeight = image.height;
+    }
+
+    const x = (image.width - newWidth) / 2;
+    const y = (image.height - newHeight) / 2;
+
+    return {
+        cropX: x,
+        cropY: y,
+        cropWidth: newWidth,
+        cropHeight: newHeight,
+    };
 }
 
 export default ImageWidget;
